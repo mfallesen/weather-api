3d14d8ffadd38b91b976f1b11172440a372a181e
Mike Fallesen <mfallesen2010@gmail.com>
2020-09-19 21:35:57 -0700

    verify function calls to APIs for both forecast and current weather

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,23 @@
 let APIKey = "bb036cc9828bfc98081575922bfed294";
 let city = "Denver"
     // Here we are building the URL we need to query the database
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+    let queryCurrentURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
+    let queryForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`;
+    let weather;
+    let forecast;
+    
     $.ajax({
-        url: queryURL,
+        url: queryCurrentURL,
         method: "GET"
       }).then(function(response){
-          console.log(response);
+          weather = response;
+        //   console.log(weather);
+      })
+      console.log(weather);
+      $.ajax({
+        url: queryForecastURL,
+        method: "GET"
+      }).then(function(response){
+          forecast = response;
+          console.log(forecast);
       })
