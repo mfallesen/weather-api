ae71893eb54ee7aa2cd11f87c9b91ae8df96b89c
Mike Fallesen <mfallesen2010@gmail.com>
2020-09-22 23:53:47 -0700

    display current weather wpoking

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,45 @@ $(document).ready(function () {
     // let city = "Denver"
     // Here we are building the URL we need to query the database
 
+    let nowDate = new Date();
+    let month = nowDate.getMonth();
+    let day = nowDate.getDate();
+    let year = nowDate.getFullYear();
+    let date = `${month}/${day}/${year}`;
+    // console.log(date);
+
     let weather;
     let forecast;
     //empty array to hold city information
-    let cityArr = [];
+
+    let cityArr = JSON.parse(localStorage.getItem("cityWeather"))||[];
+
+    if (cityArr.length) {
+        for(var i = 0; i < cityArr.length; i++) {
+            console.log(cityArr[i]);
+            addCityShortcut(cityArr[i]);
+            if(i + 1 === cityArr.length){
+                citySearch(cityArr[i]);
+            }
+        }
+    }
+
 
     $("#searchButton").click(function (event) {
         event.preventDefault();
         let city = $("#cityInput").val()
-        console.log(city);
+        if(city){
+            citySearch(city);
+        }
+    });
+
+    
+        
+        function citySearch(city) {
+        
+        $("#weatherBox").empty();
+        
+        // console.log(city);
         let queryCurrentURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
         let queryForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`;
        
@@ -23,7 +53,7 @@ $(document).ready(function () {
         }).then(function (response) {
             weather = response;
             currentWeather(weather);
-            console.log(weather);
+            // console.log(weather);
         })
         //Call the API for 5 day forecast.
         $.ajax({
@@ -34,48 +64,105 @@ $(document).ready(function () {
         })
         addCityShortcut(city);
 
-    });
+        localStorage.setItem("cityWeather", JSON.stringify(cityArr))
+
+    }
 
     function addCityShortcut(city) {
+        const idx = cityArr.indexOf(city);
+        if(idx > -1){
+            cityArr.splice(idx,1);
+            $(`#${city}`).remove();
+        }
+
         //generate button and styling dynamically for each city in the array
         let nuCity = $("<button></button>");
-        nuCity.attr("class", "form-control btn-outline-info");
+        nuCity.attr('id', city);
+        nuCity.attr("class", "form-control btn-outline-info shortcutBtn");
+        nuCity.attr("data-city", city)
         
         nuCity.text(city);
         
         if (cityArr.length < 5) {
-            cityArr.push(nuCity);
-            $("#cityList").append(nuCity);
+            cityArr.push(city);
+            $("#buttonContainer").prepend(nuCity);
         } else {
             cityArr.shift();
-            cityArr.push(nuCity);
-            $("#cityList").append(nuCity);
+            //remove the old button from the page
+            $("#buttonContainer").children().last().remove();
+            cityArr.push(city);
+            $("#buttonContainer").prepend(nuCity);
         }
         
+        $(`#${city}`).click(function (event) {
+            let city = event.currentTarget.attributes["data-city"].value;
+            $(event.currentTarget.attributes["id"]).remove();
+            // console.log(city);
+            citySearch(city);
+        });
         
-        console.log(cityArr);
+        // console.log(cityArr);
         
     };
-    console.log(cityArr);
-    function currentWeather(weather) {
-        let city = weather.name;
-        let temp = weather.main.temp;
-        let humid = weather.main.humidity;
-        let wind = weather.wind.speed;
-        let lat = weather.coord.lat;
-        let lon = weather.coord.lon;
-        let uvIndex;
+    // console.log(cityArr);
+    function currentWeather(weatherAt) {
+        let city = weatherAt.name;
+        let temp = weatherAt.main.temp;
+        let humid = weatherAt.main.humidity;
+        let wind = weatherAt.wind.speed;
+        let icon = weatherAt.weather[0].icon;
+        let iconURL =  `http://openweathermap.org/img/wn/${icon}.png`
+        let lat = weatherAt.coord.lat;
+        let lon = weatherAt.coord.lon;
+        let uvIndex = '';
         let queryUvURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${lon}`;
         
         $.ajax({
             url: queryUvURL,
             method: "GET"
         }).then(function (response) {
-             uvIndex = response;
-             console.log(uvIndex);
+             uvIndex = response.value;
+             // city UV
+                console.log(uvIndex);
+             cityUV.text(`UV Index: ${uvIndex}`);
         })
+        // build the current weather information div
+        
+        let currentWeather = $("<div>")
+        let cityHead = $("<h2>")
+        let cityWeather = $("<img>")
+        let cityTemp = $("<p>");
+        let cityHumid = $("<p>");
+        let cityWind = $("<p>");
+        let cityUV = $("<p>");
+
+        currentWeather.attr("style", "");
+        //city name date and weather icon
+        cityWeather.attr("src", iconURL);
+        cityHead.text(`${city} (${date})`);
+        cityHead.append(cityWeather)
+        currentWeather.append(cityHead);
+
+        // city Temp
+        cityTemp.text(`Temperature: ${temp} \xb0F`);
+        currentWeather.append(cityTemp);
+        
+        //city humidity
+        cityHumid.text(`Humidity: ${humid}%`);
+        currentWeather.append(cityHumid);
+
+        // city Wind 
+        cityWind.text(`Wind Speed: ${wind} MPH`);
+        currentWeather.append(cityWind);
+        // console.log(uvIndex);
+
+        currentWeather.append(cityUV);
+
+        //print to screen
+        $("#weatherBox").append(currentWeather);
 
     };
+
 
 
 
