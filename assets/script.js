370a8e6852f263ac1fe72443d352c2c2abb6c7d2
Mike Fallesen <mfallesen2010@gmail.com>
2020-09-22 23:59:22 -0700

    fixed buttons and local Storage

--- assets/script.js
+++ assets/script.js
@@ -15,14 +15,16 @@ $(document).ready(function () {
     let forecast;
     //empty array to hold city information
 
-    let cityArr = JSON.parse(localStorage.getItem("cityWeather"))||[];
+    let cityArr = JSON.parse(localStorage.getItem("cityWeather")) || [];
 
     if (cityArr.length) {
-        for(var i = 0; i < cityArr.length; i++) {
-            console.log(cityArr[i]);
-            addCityShortcut(cityArr[i]);
-            if(i + 1 === cityArr.length){
-                citySearch(cityArr[i]);
+        const cityArrCopy =[...cityArr];
+
+        for (var i = 0; i < cityArrCopy.length; i++) {
+            console.log(cityArrCopy[i]);
+            addCityShortcut(cityArrCopy[i]);
+            if (i + 1 === cityArrCopy.length) {
+                citySearch(cityArrCopy[i]);
             }
         }
     }
@@ -31,22 +33,22 @@ $(document).ready(function () {
     $("#searchButton").click(function (event) {
         event.preventDefault();
         let city = $("#cityInput").val()
-        if(city){
+        if (city) {
             citySearch(city);
         }
     });
 
-    
-        
-        function citySearch(city) {
-        
+
+
+    function citySearch(city) {
+
         $("#weatherBox").empty();
-        
+
         // console.log(city);
         let queryCurrentURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
         let queryForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`;
-       
-       //call the API for current Weather
+
+        //call the API for current Weather
         $.ajax({
             url: queryCurrentURL,
             method: "GET"
@@ -70,8 +72,8 @@ $(document).ready(function () {
 
     function addCityShortcut(city) {
         const idx = cityArr.indexOf(city);
-        if(idx > -1){
-            cityArr.splice(idx,1);
+        if (idx > -1) {
+            cityArr.splice(idx, 1);
             $(`#${city}`).remove();
         }
 
@@ -80,9 +82,9 @@ $(document).ready(function () {
         nuCity.attr('id', city);
         nuCity.attr("class", "form-control btn-outline-info shortcutBtn");
         nuCity.attr("data-city", city)
-        
+
         nuCity.text(city);
-        
+
         if (cityArr.length < 5) {
             cityArr.push(city);
             $("#buttonContainer").prepend(nuCity);
@@ -93,16 +95,16 @@ $(document).ready(function () {
             cityArr.push(city);
             $("#buttonContainer").prepend(nuCity);
         }
-        
+
         $(`#${city}`).click(function (event) {
             let city = event.currentTarget.attributes["data-city"].value;
             $(event.currentTarget.attributes["id"]).remove();
             // console.log(city);
             citySearch(city);
         });
-        
+
         // console.log(cityArr);
-        
+
     };
     // console.log(cityArr);
     function currentWeather(weatherAt) {
@@ -111,23 +113,23 @@ $(document).ready(function () {
         let humid = weatherAt.main.humidity;
         let wind = weatherAt.wind.speed;
         let icon = weatherAt.weather[0].icon;
-        let iconURL =  `http://openweathermap.org/img/wn/${icon}.png`
+        let iconURL = `http://openweathermap.org/img/wn/${icon}.png`
         let lat = weatherAt.coord.lat;
         let lon = weatherAt.coord.lon;
         let uvIndex = '';
         let queryUvURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${lon}`;
-        
+
         $.ajax({
             url: queryUvURL,
             method: "GET"
         }).then(function (response) {
-             uvIndex = response.value;
-             // city UV
-                console.log(uvIndex);
-             cityUV.text(`UV Index: ${uvIndex}`);
+            uvIndex = response.value;
+            // city UV
+            console.log(uvIndex);
+            cityUV.text(`UV Index: ${uvIndex}`);
         })
         // build the current weather information div
-        
+
         let currentWeather = $("<div>")
         let cityHead = $("<h2>")
         let cityWeather = $("<img>")
@@ -146,7 +148,7 @@ $(document).ready(function () {
         // city Temp
         cityTemp.text(`Temperature: ${temp} \xb0F`);
         currentWeather.append(cityTemp);
-        
+
         //city humidity
         cityHumid.text(`Humidity: ${humid}%`);
         currentWeather.append(cityHumid);
@@ -168,7 +170,7 @@ $(document).ready(function () {
 
 
 
-    
+
     //TODO: store returned information
     //TODO: generate clickable "button" for the returned city that stores the response to localstorage
     //TODO: generate HTML elements to render the City Temperature Humidity Wind SPeed and UV Index on screen
